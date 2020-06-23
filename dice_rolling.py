e6b9cd6cbe659d3b7071b76a4dd7b9bb39971008
joshithagandra <joshitha1830@gmail.com>
2020-06-23 13:56:25 +0530

    Created simple python program

--- dice_rolling.py
+++ dice_rolling.py
@@ -1,11 +1,23 @@
 """ The dice rolling simulator will imitate the experience of rolling a dice. It will generate a random number and the user can play again and again to get a number from the dice until the user decides to quit the program."""
 
 import random
+import time
 
-print("Press any integer to Play Again or \"0\" to Quit")
-val=input()
+print("Press y to Play or n to Quit")
+val=input().lower()
 
-while val != 0:
- print(random.randrange(1, 6))
- print("Press any integer to Play Again or \"0\" to Quit")
- val=input()
+while val != 'n':
+ print("Dice is rolling...")
+ time.sleep(1)
+ 
+ dice_1=random.randrange(1, 6)
+ print("Dice 1 value: ",dice_1)
+ dice_2=random.randrange(1, 6)
+ print("Dice 2 value: ",dice_2)
+ 
+ if dice_1 == dice_2:
+  print("Congratulations! You got same values")
+  
+ print("Press y to Play Again or n to Quit")
+ val=input().lower()
+
